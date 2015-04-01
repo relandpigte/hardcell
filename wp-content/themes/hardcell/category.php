84bbbcc4b818fa7bc84a01185742dd0a0c4576e5
Dale Ramirez <dale.ramirez@linkdigital.com.au>
2015-04-01 10:16:37 +0800

    Final changes

--- wp-content/themes/hardcell/category.php
+++ wp-content/themes/hardcell/category.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The main template file.
  *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package Hard Cell
@@ -13,7 +17,7 @@ get_header(); ?>
 			<main id="main" class="site-main" role="main">
 
 			<?php if ( have_posts() ) : ?>
-
+			
 				<header class="archive-page-header">
 					<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
